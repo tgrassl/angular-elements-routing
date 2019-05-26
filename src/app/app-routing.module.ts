5d7ac9684ad7daa943b9386e0b9773ddae2df5e4
Timon Graßl <hyperfx.official@gmail.com>
2019-05-26 11:14:27 +0200

    removed mocklocationstrategy from app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [ {provide: LocationStrategy, useClass: MultiLocationStrategy}]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
